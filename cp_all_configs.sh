0a8294cabebf7aa79640d8040f53cac5c471ac69
Jung Dong Wook <aleksanderchun24@gmail.com>
2025-03-22 14:07:09 +0300

    fixed: hammerspoon wm, added: stats sketchybar

---
+++ cp_all_configs.sh
@@ -0,0 +1,6 @@
+rm brewfile
+brew bundle dump --file=~/.dotfiles/config/brewfile
+rm -rf sketchybar
+cp -r -- ~/.config/sketchybar/. ./sketchybar
+cp ~/.zshrc .
+cp ~/.hammerspoon/init.lua
--- sketchybar/plugins/system.sh
+++ sketchybar/plugins/system.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Получаем уровень батареи
+# Battery
 BATTERY=$(pmset -g batt | grep -o '[0-9]\+%')
 
-# Получаем уровень громкости
+# Volume
 VOLUME=$(osascript -e 'output volume of (get volume settings)')
 
-# Обновляем label в SketchyBar
-sketchybar --set system_island label="Battery: ${BATTERY} | Vol: ${VOLUME}%"
+# CPU
+CPU=$(top -l 1 | grep "CPU usage" | awk '{print $3}' | sed 's/%//')
+
+# RAM usage
+RAW=$(top -l 1 | grep PhysMem)
+USED=$(echo "$RAW" | awk '{print $2}' | sed 's/M//')
+FREE=$(echo "$RAW" | awk '{print $6}' | sed 's/M//')
+TOTAL=$(echo "$USED + $FREE" | bc)
+RAM_PERC=$(echo "scale=0; 100 * $USED / $TOTAL" | bc)
+
+# SSD usage
+SSD=$(df -H /System/Volumes/Data | awk 'NR==2 {print $5}')
+
+# Вывод
+sketchybar --set system_island label="CPU: ${CPU}%   RAM: ${RAM_PERC}%   SSD: ${SSD}     ${BATTERY}     ${VOLUME}%  "
